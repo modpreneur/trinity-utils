588907048091a6dfa52171d7f5e9faa327190d67
Martin Kolek <prodejledu@gmail.com>
2016-09-19 14:48:19 +0200

    hydrators

---
+++ Hydrators/ColumnHydrator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Trinity\Component\Utils\Hydrators;
+
+use Doctrine\ORM\Internal\Hydration\AbstractHydrator;
+use PDO;
+
+/**
+ * Hydrate into simple array without column name
+ */
+class ColumnHydrator extends AbstractHydrator
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function hydrateAllData()
+    {
+        return $this->_stmt->fetchAll(PDO::FETCH_COLUMN);
+    }
+}
---
+++ Hydrators/KeyPairHydrator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Trinity\Component\Utils\Hydrators;
+
+use Doctrine\ORM\Internal\Hydration\AbstractHydrator;
+use PDO;
+
+/**
+ * Hydrate into array in format key(first parameter) => value(second parameter)
+ */
+class KeyPairHydrator extends AbstractHydrator
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function hydrateAllData()
+    {
+        return $this->_stmt->fetchAll(PDO::FETCH_KEY_PAIR);
+    }
+}
